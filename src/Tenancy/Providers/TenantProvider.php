5dcef10bbdb3870ef7acc2310d50210f1541a6c7
luceos <daniel+github@klabbers.email>
2018-11-21 13:30:34 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Tenancy/Providers/TenantProvider.php
+++ src/Tenancy/Providers/TenantProvider.php
@@ -1,8 +1,20 @@
 <?php
 
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
 namespace Tenancy\Providers;
 
- use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Foundation\Application;
  use Illuminate\Support\ServiceProvider;
  use Tenancy\Identification\Contracts\ResolvesTenants;
  use Tenancy\Identification\Contracts\Tenant;
