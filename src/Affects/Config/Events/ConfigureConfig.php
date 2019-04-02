7b839be1773e813ef441abecbb9faafb6420ecb3
Arlon Antonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-04-02 09:52:40 +0200

    Scaffolding of AffectsConfig & add subsplit

---
+++ src/Affects/Config/Events/ConfigureConfig.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Affects\Config\Events;
+
+use Tenancy\Identification\Events\Resolved;
+use Tenancy\Identification\Events\Switched;
+
+class ConfigureConfig
+{
+    /**
+     * @var Resolved|Switched
+     */
+    public $event;
+    public $config;
+
+    public function __construct($event, &$config)
+    {
+        $this->event = $event;
+        $this->config = &$config;
+    }
+}
