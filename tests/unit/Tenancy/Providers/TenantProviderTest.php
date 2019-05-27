f80f2c2f154811c929485a8badb9b21d50e1d13c
Arlon Antonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-05-27 16:31:42 +0200

    Create TenantProviderTest.php

---
+++ tests/unit/Tenancy/Providers/TenantProviderTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Tests\Unit\Providers;
+
+use Tenancy\Facades\Tenancy;
+use Tenancy\Testing\TestCase;
+use Tenancy\Identification\Contracts\Tenant;
+
+class TenantProviderTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function provider_resolves_tenant()
+    {
+        $tenant = $this->mockTenant();
+
+        $this->resolveTenant($tenant);
+
+        $this->assertEquals(
+            $tenant,
+            resolve(Tenant::class)
+        );
+    }
+}
