080b3eafb3db77e7a5379ffd99e44b9d98d5f32a
luceos <daniel+github@klabbers.email>
2019-07-01 14:52:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Tenancy/Pipeline/Steps.php
+++ src/Tenancy/Pipeline/Steps.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
 namespace Tenancy\Pipeline;
 
 use Illuminate\Support\Collection;
@@ -14,6 +26,5 @@ class Steps extends Collection
      */
     public function after($previous, Step $step)
     {
-        
     }
 }
