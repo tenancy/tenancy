15d34f135d7c3bdb81ce45ba86c3083d44f7876f
luceos <daniel+github@klabbers.email>
2019-07-02 07:53:52 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Tenancy/Identification/Support/TenantModelCollection.php
+++ src/Tenancy/Identification/Support/TenantModelCollection.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of the tenancy/tenancy package.
  *
- * (c) Daniël Klabbers <daniel@klabbers.email>
+ * Copyright Laravel Tenancy & Daniël Klabbers <daniel@klabbers.email>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
