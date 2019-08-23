e0fbad655f4767d015007747225c3c3a98faa9c9
Arlon Antonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-08-23 09:40:58 +0200

    Create & Register DatabaseResolver

---
+++ src/Hooks/Database/DatabaseResolver.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * Copyright Laravel Tenancy & Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://tenancy.dev
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Hooks\Database;
+
+use Illuminate\Support\Traits\Macroable;
+use Illuminate\Contracts\Events\Dispatcher;
+use Tenancy\Identification\Contracts\Tenant;
+use Tenancy\Hooks\Database\Contracts\ProvidesDatabase;
+use Tenancy\Hooks\Database\Contracts\ResolvesDatabases;
+
+class DatabaseResolver implements ResolvesDatabases
+{
+    use Macroable;
+
+    /**
+     * @var Dispatcher
+     */
+    protected $events;
+
+    public function __construct(Dispatcher $events)
+    {
+        $this->events = $events;
+    }
+
+    public function __invoke(Tenant $tenant = null, string $connection = null): ?ProvidesDatabase
+    {
+        /** @var ProvidesDatabase|null $provider */
+        $provider = $this->events->until(new Events\Resolving($tenant, $connection));
+
+        if ($provider) {
+            $this->events->dispatch(new Events\Identified($tenant, $connection, $provider));
+        }
+
+        $this->events->dispatch(new Events\Resolved($tenant, $connection, $provider));
+
+        return $provider;
+    }
+}
