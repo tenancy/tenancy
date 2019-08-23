a3e8a2af154882aeeb4c11c625e2013a979d0123
Arlon Antonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-08-23 09:45:41 +0200

    Create Additional Database Listeners

---
+++ src/Database/Mysql/Listeners/ConfiguresTenantConnection.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * Copyright Laravel Tenancy & Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://tenancy.dev
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Database\Drivers\Mysql\Listeners;
+
+use Tenancy\Identification\Contracts\Tenant;
+use Tenancy\Affects\Connection\Events\Resolving;
+use Tenancy\Affects\Connection\Events\Drivers\Configuring;
+use Tenancy\Affects\Connection\Contracts\ProvidesConfiguration;
+
+class ConfiguresTenantConnection implements ProvidesConfiguration
+{
+    public function handle(Resolving $resolving): ?ProvidesConfiguration
+    {
+        return $this;
+    }
+
+    public function configure(Tenant $tenant): array{
+        $config = [];
+
+        $this->events()->dispatch(new Configuring($tenant, $config, $this));
+
+        return $config;
+    }
+}
