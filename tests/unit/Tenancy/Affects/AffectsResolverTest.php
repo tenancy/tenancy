8f6c3735bfde0b8e45bc3555a73c04f8a7660383
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2019-08-23 16:58:31 +0200

    Add Affects Test

---
+++ tests/unit/Tenancy/Affects/AffectsResolverTest.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * Copyright Laravel Tenancy & Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://tenancy.dev
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Tests\Affects;
+
+use InvalidArgumentException;
+use Tenancy\Testing\TestCase;
+use Tenancy\Affects\Contracts\ResolvesAffects;
+
+class HookResolverTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function validates_affectss()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        /** @var ResolvesAffects $resolver */
+        $resolver = resolve(ResolvesAffects::class);
+        $resolver->addAffect(Mocks\InvalidAffects::class);
+    }
+
+    /**
+     * @test
+     */
+    public function sets_affects()
+    {
+        /** @var ResolvesAffects $resolver */
+        $resolver = resolve(ResolvesAffects::class);
+
+        $resolver->setAffects([]);
+        $this->assertEquals(
+            [],
+            $resolver->getAffects()
+        );
+
+        $resolver->setAffects([
+            Mocks\ValidAffects::class,
+        ]);
+
+        $this->assertEquals(
+            [Mocks\ValidAffects::class],
+            $resolver->getAffects()
+        );
+    }
+}
