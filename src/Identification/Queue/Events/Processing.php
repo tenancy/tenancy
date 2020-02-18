b8a8df20fa06379a0d32bdf816332ef1d05d1d64
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2020-02-18 18:27:53 +0100

    Change order

--- src/Identification/Queue/Events/Processing.php
+++ src/Identification/Queue/Events/Processing.php
@@ -57,8 +57,8 @@ class Processing
 
         /** @var array $payload */
         $payload = $event->job->payload();
-
         $job = null;
+
         if ($command = Arr::get($payload, 'data.command')) {
             $job = $this->unserializeToJob($command);
         }
