962931c00597bc035429d30ffafec97aff104af8
Eric Fletcher <eric@thefletcher.net>
2020-04-27 22:58:07 -0400

    Because why not.

--- tests/Affects/Mails/Unit/ConfigureMailsTest.php
+++ tests/Affects/Mails/Unit/ConfigureMailsTest.php
@@ -16,8 +16,11 @@ declare(strict_types=1);
 
 namespace Tenancy\Tests\Affects\Mails\Unit;
 
+use Illuminate\Mail\Transport\ArrayTransport;
+use Mockery;
 use Tenancy\Affects\Mails\Events\ConfigureMails;
 use Tenancy\Affects\Mails\Provider;
+use Tenancy\Facades\Tenancy;
 use Tenancy\Tests\Affects\AffectsEventUnitTestCase;
 
 class ConfigureMailsTest extends AffectsEventUnitTestCase
@@ -25,4 +28,37 @@ class ConfigureMailsTest extends AffectsEventUnitTestCase
     protected $affectsProvider = Provider::class;
 
     protected $event = ConfigureMails::class;
+
+    /** @test */
+    public function the_event_can_replace_swiftmailer()
+    {
+        $this->app->register($this->affectsProvider);
+
+        $this->events->listen($this->event, function (ConfigureMails $event) {
+            $event->replaceSwiftMailer(new ArrayTransport());
+
+            $this->assertInstanceOf(ArrayTransport::class, $event->mailer->getSwiftMailer()->getTransport());
+        });
+
+        Tenancy::setTenant($this->tenant);
+    }
+
+    /** @test */
+    public function the_event_can_delegate_to_mailer()
+    {
+
+        $this->app->register($this->affectsProvider);
+
+        $this->events->listen($this->event, function (ConfigureMails $e) {
+            $mailerSpy = Mockery::spy(get_class($e->mailer));
+
+            $e->mailer = $mailerSpy;
+
+            $e->to('test@example.com');
+
+            $mailerSpy->shouldHaveReceived()->to('test@example.com');
+        });
+
+        Tenancy::setTenant($this->tenant);
+    }
 }
