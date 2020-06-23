2ee7ca46f52314d114f67185ad1870b3e44e9baf
Eric Fletcher <exf7630@rit.edu>
2020-06-23 01:15:09 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/SDK/resources/configs/sdk.php
+++ src/SDK/resources/configs/sdk.php
@@ -1,7 +1,21 @@
 <?php
 
-use Tenancy\SDK\Identification\HTTP\Mode as HTTP;
+declare(strict_types=1);
+
+/*
+ * This file is part of the tenancy/tenancy package.
+ *
+ * Copyright Tenancy for Laravel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://tenancy.dev
+ * @see https://github.com/tenancy
+ */
+
 use Tenancy\SDK\Identification\Console\Mode as Console;
+use Tenancy\SDK\Identification\HTTP\Mode as HTTP;
 use Tenancy\SDK\Identification\Queue\Mode as Queue;
 
 return [
@@ -17,12 +31,12 @@ return [
         ],
         'console' => [
             /**
-             * The mode for console identification
+             * The mode for console identification.
              */
             'mode' => Console::DUMP,
         ],
         'queue' => [
             'mode' => Queue::DUMP,
-        ]
-    ]
+        ],
+    ],
 ];
