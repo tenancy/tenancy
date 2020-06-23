dcaa780730d3fb338a49b3fa1c94acc5b39f8afc
Eric Fletcher <eric@thefletcher.net>
2020-06-22 22:30:33 -0400

    Add flush method to ConfigureMigrations and stores configured paths locally to the MigratesHook

--- tests/Hooks/Migration/Unit/ConfiguresMigrationsTest.php
+++ tests/Hooks/Migration/Unit/ConfiguresMigrationsTest.php
@@ -29,6 +29,12 @@ class ConfiguresMigrationsTest extends ConfigureHookTestCase
 
     protected $eventClass = ConfigureMigrations::class;
 
+    protected function afterSetUp()
+    {
+        resolve('migrator')->path(__DIR__);
+        parent::afterSetUp();
+    }
+
     /**
      * @dataProvider tenantEventsProvider
      *
@@ -36,14 +42,37 @@ class ConfiguresMigrationsTest extends ConfigureHookTestCase
     public function it_can_add_paths_to_the_migrator($tenantEvent)
     {
         $this->events->listen($this->eventClass, function ($event) {
-            $event->path(__DIR__);
+            $event->path(realpath(__DIR__.'/..'));
         });
 
         $this->hook->for(new $tenantEvent($this->mockTenant()));
 
         $this->assertContains(
+            realpath(__DIR__.'/..'),
+            $this->hook->paths
+        );
+    }
+
+    /**
+     * @dataProvider tenantEventsProvider
+     *
+     * @test */
+    public function it_can_clear_paths_from_the_migrator($tenantEvent)
+    {
+        $this->assertContains(
             __DIR__,
-            $this->hook->migrator->paths()
+            $this->hook->paths
+        );
+        
+        $this->events->listen($this->eventClass, function ($event) {
+            $event->flush();
+        });
+
+        $this->hook->for(new $tenantEvent($this->mockTenant()));
+
+        $this->assertNotContains(
+            __DIR__,
+            $this->hook->paths
         );
     }
 }
