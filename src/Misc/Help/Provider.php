f7a7992640b84b75914f7941c443572854e4d8ae
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2020-06-24 17:00:00 +0200

    Revert "Uncomment all packages from provider for now"
    This reverts commit d58bca4efdc6baa45dbaa6994cf48928d1033c85.

--- src/Misc/Help/Provider.php
+++ src/Misc/Help/Provider.php
@@ -37,25 +37,25 @@ class Provider extends SupportProvider
             $resolver->registerPackage(new Main\Framework());
 
             // All affects
-            // $resolver->registerPackage(new Affects\Broadcasts());
-            // $resolver->registerPackage(new Affects\Cache());
-            // $resolver->registerPackage(new Affects\Configs());
-            // $resolver->registerPackage(new Affects\Connections());
-            // $resolver->registerPackage(new Affects\Filesystems());
-            // $resolver->registerPackage(new Affects\Logs());
-            // $resolver->registerPackage(new Affects\Mails());
-            // $resolver->registerPackage(new Affects\Models());
-            // $resolver->registerPackage(new Affects\Routes());
-            // $resolver->registerPackage(new Affects\Urls());
-            // $resolver->registerPackage(new Affects\Views());
+            $resolver->registerPackage(new Affects\Broadcasts());
+            $resolver->registerPackage(new Affects\Cache());
+            $resolver->registerPackage(new Affects\Configs());
+            $resolver->registerPackage(new Affects\Connections());
+            $resolver->registerPackage(new Affects\Filesystems());
+            $resolver->registerPackage(new Affects\Logs());
+            $resolver->registerPackage(new Affects\Mails());
+            $resolver->registerPackage(new Affects\Models());
+            $resolver->registerPackage(new Affects\Routes());
+            $resolver->registerPackage(new Affects\Urls());
+            $resolver->registerPackage(new Affects\Views());
 
-            // // All Hooks
-            // $resolver->registerPackage(new Hooks\Database());
-            // $resolver->registerPackage(new Hooks\Migration());
+            // All Hooks
+            $resolver->registerPackage(new Hooks\Database());
+            $resolver->registerPackage(new Hooks\Migration());
 
-            // // All Database Drivers
-            // $resolver->registerPackage(new Database\Mysql());
-            // $resolver->registerPackage(new Database\Sqlite());
+            // All Database Drivers
+            $resolver->registerPackage(new Database\Mysql());
+            $resolver->registerPackage(new Database\Sqlite());
 
             return $resolver;
         });
