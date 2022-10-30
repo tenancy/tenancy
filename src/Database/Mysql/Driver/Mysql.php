bfefd7b01ee0a5d70da74a290b8ca5c11ecb1b78
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2022-10-30 09:44:55 +0100

    Make sure the tenant connection is not used for moving tables

--- src/Database/Mysql/Driver/Mysql.php
+++ src/Database/Mysql/Driver/Mysql.php
@@ -19,6 +19,7 @@ namespace Tenancy\Database\Drivers\Mysql\Driver;
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Tenancy\Affects\Connections\Contracts\ResolvesConnections;
 use Tenancy\Database\Drivers\Mysql\Concerns\ManagesSystemConnection;
 use Tenancy\Facades\Tenancy;
@@ -136,13 +137,15 @@ class Mysql implements ProvidesDatabase
         $tempTenant = $tenant->replicate();
         $tempTenant->{$tenant->getTenantKeyName()} = $tenant->getOriginal($tenant->getTenantKeyName());
 
+        $tempConnectionName = Str::random(16);
+
         /** @var ResolvesConnections $resolver */
         $resolver = resolve(ResolvesConnections::class);
-        $resolver($tempTenant, Tenancy::getTenantConnectionName());
+        $resolver($tempTenant, $tempConnectionName);
 
-        $tables = Tenancy::getTenantConnection()->getDoctrineSchemaManager()->listTableNames();
+        $tables = DB::connection($tempConnectionName)->getDoctrineSchemaManager()->listTableNames();
 
-        $resolver(null, Tenancy::getTenantConnectionName());
+        $resolver(null, $tempConnectionName);
 
         return $tables;
     }
