90fd1c38ec1134aeb7901061850c4a6cbcd5d682
Arlon Antonius <37894143+ArlonAntonius@users.noreply.github.com>
2023-10-26 05:42:15 +0200

    Include tests

--- tests/Hooks/Migration/Unit/ConfiguresSeedsTest.php
+++ tests/Hooks/Migration/Unit/ConfiguresSeedsTest.php
@@ -46,4 +46,23 @@ class ConfiguresSeedsTest extends ConfigureHookTestCase
             $this->hook->seeds
         );
     }
+
+    /**
+     * @dataProvider tenantEventsProvider
+     *
+     * @test */
+    public function it_can_decide_whether_to_replace_the_default($tenantEvent)
+    {
+        $this->events->listen($this->eventClass, function ($event) {
+            $event->hook->withDefaultConnection(false);
+        });
+
+        $this->hook->for(new $tenantEvent($this->mockTenant()));
+
+        $reflection = new \ReflectionClass($this->hook);
+
+        $this->assertFalse(
+            $reflection->getProperty('replaceDefaultConnection')->getValue($this->hook)
+        );
+    }
 }
